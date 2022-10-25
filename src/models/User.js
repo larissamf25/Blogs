e9130d97e39bc3fc4f93c22f74ee58aa806b7282
Larissa <larissamf25@gmail.com>
2022-10-25 15:25:06 -0300

    feat: req 02

---
+++ src/models/User.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+    tableName: 'users',
+    underscored: true,
+  });
+
+  /*User.associate = (models) => {
+    User.hasMany(models.BlogPosts,
+      { foreignKey: 'user_id', as: 'posts' });
+  };*/
+
+  return User;
+};
